6015d753907121038434c7ee7ff8d9bd4c55fe02
Bishwajyoti Chaudhary <bishwajyotichaudhary46@gmail.com>
2023-06-27 16:40:03 +0545

    Data Ingestion sucessfully Done

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SMDF
 Demand Forecasting
+
+# Work Flow
+
+1. Update Config.yaml
+2. Update Params.yaml
+3. Update entity
+4. Update the configuration manager in src config
+5. Update the component
+6. Update the pipeline
+7. Update the main.py
+8. Update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from SMDF.pipeline.stage_01_dataingestion import DataIngestionTrainingPipeline
 from SMDF.logging import logger
 
-logger.info("Welcome to my Custom Logger")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} sucessfully completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
