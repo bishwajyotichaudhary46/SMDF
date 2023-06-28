22844b4eab1666e0a9fa3e482b218946bf44f4f1
Bishwajyoti Chaudhary <bishwajyotichaudhary46@gmail.com>
2023-06-28 08:23:08 +0545

    data validation and some update in data ingestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from SMDF.pipeline.stage_01_dataingestion import DataIngestionTrainingPipeline
 from SMDF.logging import logger
+from SMDF.pipeline.stage_02_datavalidation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -9,5 +10,16 @@ try:
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} sucessfully completed <<<<<<\n\nx==========x")
 except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        datavalidatin = DataValidationTrainingPipeline()
+        datavalidatin.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
         logger.exception(e)
         raise e
